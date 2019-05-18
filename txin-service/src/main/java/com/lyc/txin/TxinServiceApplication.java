539a5bb7baec48eae4e33ab8f4bf726cbfc2a1da
lyc <335171398@qq.com>
2019-05-18 09:36:58 +0800

    Initial commit

---
+++ txin-service/src/main/java/com/lyc/txin/TxinServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lyc.txin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxinServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxinServiceApplication.class, args);
+	}
+
+}
---
+++ txin-web/src/main/java/com/lyc/txin/TxinWebApplication.java
@@ -0,0 +1,13 @@
+package com.lyc.txin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxinWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxinWebApplication.class, args);
+	}
+
+}
