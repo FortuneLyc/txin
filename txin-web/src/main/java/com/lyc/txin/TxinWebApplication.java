519311e285f242ab045f1ab911f460373ac6088b
lyc <335171398@qq.com>
2019-05-28 00:46:17 +0800

    edit

--- txin-web/src/main/java/com/lyc/txin/TxinWebApplication.java
+++ txin-web/src/main/java/com/lyc/txin/TxinWebApplication.java
@@ -2,8 +2,13 @@ package com.lyc.txin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.act"},exclude={JacksonAutoConfiguration.class})
+@ServletComponentScan(basePackages = "com.act")
+@EnableCaching
 public class TxinWebApplication {
 
 	public static void main(String[] args) {
